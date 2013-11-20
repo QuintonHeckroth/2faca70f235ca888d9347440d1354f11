f72790b99e90525a99e66887250f2492c66ff0c2
ProfGent <parodius_king@hotmail.com>
2013-11-20 03:37:45 +0000

    Started Boy Script

---
+++ Wolfs Tale/Assets/Scripts/BoyController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoyController : MonoBehaviour {
+
+	int horizontalAxis;
+	public float boyMoveForce = 10;
+	public float boyJumpForce = 10;
+
+	// Use this for initialization
+	void Start ()
+	{
+		horizontalAxis = (int)Input.GetAxis("Horizontal");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void FixedUpdate ()
+	{
+		switch(horizontalAxis)
+		{
+		case 0:
+		break;
+		case -1:
+			rigidbody2D.AddForce(Vector2.right * horizontalAxis * boyMoveForce);
+		break;
+		case 1:
+			rigidbody2D.AddForce(Vector2.right * horizontalAxis * boyMoveForce);
+		break;
+		default:
+		break;
+		}
+	}
+}
